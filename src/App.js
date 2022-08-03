681f597cd66c0b4922aee5b077682e6b35f021eb
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2022-08-03 15:55:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Component/Header/Navbar';
 import './App.css';
+import MainHeader from './Component/Header/MainHeader';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <MainHeader />
     </div>
   );
 }
