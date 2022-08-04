f11ed760f3539c56d9cf9e60de0742743ea8fd2e
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2022-08-04 08:30:37 +0530

    Update RouteMain.jsx

--- src/RouteMain.jsx
+++ src/RouteMain.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import App from "./App";
 import NewEmail from "./Component/Dashboard/NewEmail";
+import EmailWizard from "./Component/Dashboard/EmailWizard";
 import Login from "./Component/Header/Login";
 import Register from "./Component/Header/Register";
 
@@ -13,6 +14,8 @@ function RouteMain() {
         <Route path="/login" exact element={<Login />} />
         <Route path="/signup" exact element={<Register />} />
         <Route path="/dash" exact element={<NewEmail />} />
+        <Route path="/new" exact element={<EmailWizard />} />
+
       </Routes>
    
   );
