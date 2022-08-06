53c4486455d36baae10581c9036e2dbfaf49980d
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2022-08-06 13:51:54 +0530

    Update RouteMain.jsx

--- src/RouteMain.jsx
+++ src/RouteMain.jsx
@@ -1,21 +1,60 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Component } from "react";
+import { Routes, Route } from "react-router-dom";
 import App from "./App";
 import NewEmail from "./Component/Dashboard/NewEmail";
 import EmailWizard from "./Component/Dashboard/EmailWizard";
 import Login from "./Component/Header/Login";
 import Register from "./Component/Header/Register";
+import { auth } from "./Config/fire";
+import { onAuthStateChanged } from "firebase/auth";
 
-function RouteMain() {
-  return (
+class RouteMain extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+  authListener() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
+  render() {
+    return (
       <Routes>
-        <Route path="/" exact element={<App />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/signup" exact element={<Register />} />
-        <Route path="/dash" exact element={<NewEmail />} />
-        <Route path="/new" exact element={<EmailWizard />} />
+         {(() => {
+          if (this.state.user) {
+            return (
+              <>
+                <Route path="/" exact element={<App />} />
+                <Route path="/dash" exact element={<NewEmail />} />
+                <Route path="/new" exact element={<EmailWizard />} />
+              </>
+            )
+          } else {
+            return (
+              <>
+                <Route path="/" exact element={<App />} />
+                <Route path="/login" exact element={<Login />} />
+                <Route path="/signup" exact element={<Register />} />
+              </>
+            );
+          }
+        })()}
       </Routes>
-  );
+    );
+  }
 }
 
 export default RouteMain;
